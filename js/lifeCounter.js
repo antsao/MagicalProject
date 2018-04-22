a741e6194289cca44787de31a8011e7393e4b09f
antsao <antsao@calpoly.edu>
2018-04-22 02:41:12 -0700

    html and css enhancements

--- js/lifeCounter.js
+++ js/lifeCounter.js
@@ -1,6 +1,22 @@
+var player1Life;
+var player2Life;
+var player3Life;
+var player4Life;
+var player5Life;
+var player6Life;
+var player7Life;
+var player8life;
+
+function addClickPlayer1() {
+  $("#player1").html("" + ++player1Life);
+}
+
 function main() {
   var islandImageUrl = "./img/island.png";
   var forestImageUrl = "./img/forest.png";
+
+  var player1Life = 20;
+  var player2Life = 20;
 
   $("#player1").css("background-image", "url(" + islandImageUrl + ")");
   $("#player1").css("background-size", "cover");
